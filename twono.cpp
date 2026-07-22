3f0b743173051c6fa17176b76fd8083eac96e426
desaiatharv65-sketch <desaiatharv65@gmail.com>
2026-07-22 04:28:14 +0000

    stop

---
+++ twono.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int sum,num1,num2;
+  cout<<"enter frist number:";
+  cin>> num1;
+  cout<<"enter second number:";
+  cin>> num2;
+  sum = num1 + num2;
+  cout<<" sum = " << sum << endl;
+  return 0;
+}
