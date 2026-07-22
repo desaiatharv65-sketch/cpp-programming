2470b62fa4bbdf6fcdd02f6638957a8e33059f84
desaiatharv65-sketch <desaiatharv65@gmail.com>
2026-07-22 04:50:24 +0000

    hi

---
+++ anp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,w;
+    cout<<"enter length and width : ";
+    cin>>l>>w;
+    cout<< "area = " << l*w << endl;
+    cout<< "perimeter = " << 2*(l+w) << endl;
+    return 0;
+}
